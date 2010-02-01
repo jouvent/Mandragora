9a505c668b3f9f1ab8bfd736e7d9a4e90207a12b
Daniel LeBlanc <danidou@gmail.com>
2010-01-31 21:18:54 -0500

    Initial project structure
    Empty initial project structure.  A long road ahead of us.
    
    Signed-off-by: Daniel LeBlanc <danidou@gmail.com>

---
+++ includes/lib/common/string.php
@@ -0,0 +1,20 @@
+<?php
+
+define('SALT_LENGTH', 24);
+
+function generateHash($plainText, $salt = null)
+{
+    if ($salt === null)
+    {
+        $salt = substr(sha1(uniqid(rand(), true)), 0, SALT_LENGTH);
+    }
+    else
+    {
+        $salt = substr($salt, 0, SALT_LENGTH);
+    }
+
+    return $salt . sha1($salt . $plainText);
+}
+
+
+?>
