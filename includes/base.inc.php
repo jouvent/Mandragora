c88fd37687071f105ac1d5e0ac44ad89e91cf11a
Daniel LeBlanc <danidou@gmail.com>
2010-02-06 16:06:17 -0500

    Signed-off-by: Daniel LeBlanc <danidou@gmail.com>

--- includes/base.inc.php
+++ includes/base.inc.php
@@ -3,8 +3,19 @@
  * centralize the project includes
  */
 
-require_once 'control/dispatcher.php';
-require_once 'includes/lib/common/include_logic.php';
+/* Frequently used functions and classes (ex: database layer) */
+require_once('includes/lib/common/include_logic.php');
+require_once('includes/lib/database/datasource.php');
+
+/* Configuration include file */
+require_once('includes/lib/config.inc.php');
+
+/* Database models (VOs) */
+require_once('includes/lib/model/user.php');
+
+/* Data Access Objects (DAOs) */
+require_once('includes/lib/dao/user_dao.php');
+
 
 
 ?>
